09cc28feeb57e6d46046b520fea576b3500ddb0b
Tianhe Wang <tianhe.wang@airbnb.com>
2016-08-04 12:27:58 -0700

    image done

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AFNetworking
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -64,6 +65,10 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
+        let baseUrl = "https://image.tmdb.org/t/p/w342"
+        let posterPath = movie["poster_path"] as! String
+        let imageUrl = NSURL (string: baseUrl + posterPath)
+        cell.posterView.setImageWithURL(imageUrl!)
 
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
